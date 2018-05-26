98f93acf68e1130bc8bba9568ba8fcfd387aca2e
clkl.kailin <clkl.kailin@gmail.com>
2018-05-26 11:06:21 -0400

    Fixed incorrect main class causing program to fail

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
 
             users_guess = _input(line_message)
             try:
-                game = guess_letter(game, users_guess)
+                guess_letter(game, users_guess)
             except InvalidGuessedLetterException:
                 print("\t Your guess is incorrect. Please guess again.")
                 continue
